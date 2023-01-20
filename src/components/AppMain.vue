cdda2684912df4bd72a36186c63a8aced9d8364e
paoloevan <paoloev1900@gmail.com>
2023-01-20 17:32:34 +0100

    add ProjectCard component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,12 @@
 <script>
 import { state } from '../state'
+import ProjectCard from './ProjectCard.vue'
 
 export default {
     name: 'AppMain',
+    components: {
+        ProjectCard
+    },
 
     data() {
         return {
@@ -16,22 +20,7 @@ export default {
 </script>
 
 <template>
-    <div class="container my-4">
-        <div class="row row-cols-3 g-5">
-            <div v-for="project in state.projects.data" class="col">
-                <div class="card">
-                    <img class="card-img-top" :src="state.imagePath(project.cover_image)" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">{{ project.title }}</h4>
-                        <router-link :to="{ name: 'project', params: { slug_title: project.slug_title } }">
-                            Show more...
-                        </router-link>
-                        <p class="card-text">{{ project.description }}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <ProjectCard :projects="state.projects.data" :image="state.imagePath" />
 
     <nav aria-label="Page navigation">
         <ul class="pagination   justify-content-center">
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,17 +1,32 @@
 <script>
+import { state } from '../state';
+
 export default {
-    name: 'ProjectCard'
+    name: 'ProjectCard',
+    props: {
+        projects: Array,
+        image: Function
+    },
+
+    data() {
+        return {
+            state
+        }
+    }
 }
 </script>
 
 <template>
     <div class="container my-4">
-        <div v-if="!loading" class="row row-cols-3 g-5">
-            <div v-for="project in projects.data" class="col">
+        <div class="row row-cols-3 g-5">
+            <div v-for="project in projects" class="col">
                 <div class="card">
-                    <img class="card-img-top" :src="imagePath(project.cover_image)" alt="">
+                    <img class="card-img-top" :src="image(project.cover_image)" alt="">
                     <div class="card-body">
                         <h4 class="card-title">{{ project.title }}</h4>
+                        <router-link :to="{ name: 'project', params: { slug_title: project.slug_title } }">
+                            Show more...
+                        </router-link>
                         <p class="card-text">{{ project.description }}</p>
                     </div>
                 </div>
