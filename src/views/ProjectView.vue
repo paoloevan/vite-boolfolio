e412d381cf0b477a40e92dde53dd43c02584f42c
paoloevan <paoloev1900@gmail.com>
2023-01-20 16:28:33 +0100

    add single project template

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -6,7 +6,7 @@ export default {
     data() {
         return {
             baseUrl: 'http://127.0.0.1:8000/',
-            projects: {},
+            project: {},
             loading: true
         }
     },
@@ -16,12 +16,23 @@ export default {
             axios
                 .get(url)
                 .then(response => {
-                    console.log(response);
+                    // console.log(response.data.results);
+                    this.project = response.data.results
+                    this.loading = false
                 })
                 .catch(error => {
                     console.log(error.message)
                 })
-        }
+        },
+        imagePath(url) {
+            // console.log(url);
+            if (url && url.startsWith('uploads')) {
+
+                return this.baseUrl + 'storage/' + url
+            } else {
+                return 'https://via.placeholder.com/300x200'
+            }
+        },
     },
     mounted() {
         this.getProject()
@@ -30,7 +41,17 @@ export default {
 </script>
 
 <template>
-    <h1>Proj</h1>
+    <h1>{{ $route.params.slug_title }}</h1>
+
+    <div v-if="!loading" class="single-project container">
+        <div class="card">
+            <img class="card-img-top" :src="imagePath(project.cover_image)" alt="{{project.title}}">
+            <div class="card-body">
+                <h4 class="card-title">{{ project.title }}</h4>
+                <p class="card-text">{{ project.description }}</p>
+            </div>
+        </div>
+    </div>
 
 
 </template>
